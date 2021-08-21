cbb34c17b006935fc91dc811e42625f66b27d803
karlygach <karygachkenenbaeva@gmail.com>
2021-08-21 11:45:00 +0600

    включен юрл на вьюшку UnModeratedAdvertListView

--- exam_project/webapp/urls.py
+++ exam_project/webapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from webapp.views import ModeratedAdvertListView
+from webapp.views import ModeratedAdvertListView, UnModeratedAdvertListView
 
 app_name = 'adverts'
 
 
 urlpatterns = [
-    path('', ModeratedAdvertListView.as_view(), name='moderated-list')
+    path('', ModeratedAdvertListView.as_view(), name='moderated-list'),
+    path('unmoderated/', UnModeratedAdvertListView.as_view(), name='unmoderated-list'),
 ]
