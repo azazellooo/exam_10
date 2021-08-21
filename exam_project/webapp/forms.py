b5826772d3bc0eac612ebdd0b9bbc8b8db4e04e2
karlygach <karygachkenenbaeva@gmail.com>
2021-08-21 17:56:51 +0600

    добавлена форм для поиска

---
+++ exam_project/webapp/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+from webapp.models import Advert
+
+
+class AdvertForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(AdvertForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control', 'style': 'width: 300px;'})
+
+    class Meta:
+        model = Advert
+        fields = ['title', 'description', 'category', 'picture', 'price']
+
+
+class SearchForm(forms.Form):
+    search_value = forms.CharField(max_length=100, required=False, label='Search')
+
+    def __init__(self, *args, **kwargs):
+        super(SearchForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control', 'style': 'width: 300px;'})
