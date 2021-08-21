2bffc8bfcdf301fbeb5bf29290647433a62e1d30
karlygach <karygachkenenbaeva@gmail.com>
2021-08-21 10:42:15 +0600

    в админке зарегистрированы модели категорий и объявлений.

--- exam_project/webapp/admin.py
+++ exam_project/webapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from webapp.models import Category, Advert
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'category_name']
+    list_filter = ['category_name']
+    fields = ['category_name']
+
+
+@admin.register(Advert)
+class AdvertAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at', 'author', 'is_moderated', 'published_at']
+    list_filter = ['author', 'created_at', 'is_moderated', 'is_rejected']
+    fields = ['title', 'description', 'picture', 'author', 'price', 'category']
+    readonly_fields = ['created_at', 'id']
+
+
 # Register your models here.
