b20a18cd5f3419a49c26a827536a75fb158df854
karlygach <karygachkenenbaeva@gmail.com>
2021-08-21 10:41:35 +0600

    в админке зарегистрирована модель профиля

--- exam_project/accounts/admin.py
+++ exam_project/accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from accounts.models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'phone_number']
+    fields = ['phone_number', 'user']
+    readonly_fields = ['id']
+
+
 # Register your models here.
