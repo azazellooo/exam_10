7aae268be7d81937b27a796348abfe86985cb166
karlygach <karygachkenenbaeva@gmail.com>
2021-08-21 17:26:10 +0600

    написана вьюшка обновляющая номер телефона пользователя

--- exam_project/accounts/urls.py
+++ exam_project/accounts/urls.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import UserDetailView
+from accounts.views import UserDetailView, PhoneNumberUpdateView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('<int:pk>/', UserDetailView.as_view(), name='profile')
+    path('<int:pk>/', UserDetailView.as_view(), name='profile'),
+    path('<int:pk>/update', PhoneNumberUpdateView.as_view(), name='update')
 ]
--- exam_project/accounts/views.py
+++ exam_project/accounts/views.py
@@ -1,5 +1,10 @@
 from django.contrib.auth import get_user_model
-from django.views.generic import DetailView
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.urls import reverse
+from django.views.generic import DetailView, UpdateView
+
+from accounts.forms import ProfileForm
+from accounts.models import Profile
 
 
 class UserDetailView(DetailView):
@@ -15,5 +20,19 @@ class UserDetailView(DetailView):
             return context
         context['adverts'] = page_owner.advert.all()
         return context
+
+
+class PhoneNumberUpdateView(PermissionRequiredMixin, UpdateView):
+    model = Profile
+    template_name = 'registration/update_profile.html'
+    form_class = ProfileForm
+    context_object_name = 'profile'
+    permission_required = 'account.change_profile'
+
+    def has_permission(self):
+        return self.get_object().user == self.request.user
+
+    def get_success_url(self):
+        return reverse('accounts:profile', kwargs={'pk': self.kwargs.get('pk')})
 
 # Create your views here.
