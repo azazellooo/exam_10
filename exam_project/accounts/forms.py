bd82adafc565e54677cedcad3d74600a2983bd35
karlygach <karygachkenenbaeva@gmail.com>
2021-10-23 23:11:45 +0600

    изменены формы - добавлены стили

---
+++ exam_project/accounts/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from accounts.models import Profile
+
+
+class ProfileForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control w-100'})
+
+    class Meta:
+        model = Profile
+        fields = ['phone_number']
--- exam_project/webapp/forms.py
+++ exam_project/webapp/forms.py
@@ -8,7 +8,7 @@ class AdvertForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(AdvertForm, self).__init__(*args, **kwargs)
         for field in self.fields:
-            self.fields[field].widget.attrs.update({'class': 'form-control', 'style': 'width: 300px;'})
+            self.fields[field].widget.attrs.update({'class': 'form-control', 'style': 'width: 300px; margin-top: 0'})
 
     class Meta:
         model = Advert
@@ -21,4 +21,4 @@ class SearchForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(SearchForm, self).__init__(*args, **kwargs)
         for field in self.fields:
-            self.fields[field].widget.attrs.update({'class': 'form-control', 'style': 'width: 300px;'})
+            self.fields[field].widget.attrs.update({'class': 'search-input', 'placeholder': 'search'})
